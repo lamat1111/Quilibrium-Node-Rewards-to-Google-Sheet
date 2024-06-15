21fdcfd0760f1ed9bc23c6200b249fcb6ce7cca1
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-15 10:33:57 +0200

    Update installer_custom.sh

--- installer_custom.sh
+++ installer_custom.sh
@@ -2,17 +2,10 @@
 
 echo ""
 echo "This script will create an automation to populate your GSheet with QUIL hourly rewards for each node"
-echo "Remember to upload your .json authentication file for this to work"
+echo "This is a custom version and will not work for you unless you modify it"
 echo ""
-sleep 3
-
-echo "Installing Python 3 and pip3..."
-sudo apt install -y python3 python3-pip || { echo "❌ Failed to install Python 3 and pip3."; exit 1; }
 sleep 1
 
-echo "Installing required Python packages..."
-pip3 install gspread oauth2client || { echo "❌ Failed to install required Python packages."; exit 1; }
-sleep 1
 
 echo "Grabbing the script..."
 
@@ -25,39 +18,16 @@ sleep 1
 # Ask user for start column letter
 read -p "Enter the start column letter to populate (e.g., A, B, C...): " START_COLUMN
 
-# Update START_COLUMN in the script
-sed -i "s/START_COLUMN = .*/START_COLUMN = \"$START_COLUMN\"/" ~/scripts/qnode_rewards_to_gsheet.py || { echo "❌ Failed to update the START_COLUMN in the script."; exit 1; }
+# Create .config file
+~/scripts/qnode_rewards_to_gsheet.config
 
-# Function to handle errors
-handle_error() {
-    echo "❌ An error occurred while updating cron jobs."
-    exit 1
-}
+with these contents
+SHEET_NAME=Quilibrium nodes
+SHEET_TAB_NAME=Rewards
+START_COLUMN=$START_COLUMN
+START_ROW=4
 
-# Cron command to execute
-CRON_COMMAND="/usr/bin/python3 ~/scripts/qnode_rewards_to_gsheet.py"
-
-# Check if a cron job containing the command exists
-EXISTING_CRON_JOB=$(crontab -l | grep -F "$CRON_COMMAND")
-
-# If it exists, delete the existing cron job
-if [ -n "$EXISTING_CRON_JOB" ]; then
-    echo "🔄 Deleting existing cron job..."
-    (crontab -l | grep -vF "$CRON_COMMAND") | crontab - || handle_error
-    echo "✅ Existing cron job deleted successfully."
-fi
-
-# Generate a random minute between 0 and 59
-RANDOM_MINUTE=$(shuf -i 0-59 -n 1)
-
-# New cron job with a random minute every hour
-NEW_CRON_JOB="$RANDOM_MINUTE * * * * $CRON_COMMAND"
-
-# Add the new cron job
-echo "➕ Adding the new cron job..."
-(crontab -l ; echo "$NEW_CRON_JOB") | crontab - || handle_error
-echo "✅ New cron job added successfully:"
-echo "$NEW_CRON_JOB"
+chmod +x ~/scripts/qnode_rewards_to_gsheet.config
 
 
 sleep 1
