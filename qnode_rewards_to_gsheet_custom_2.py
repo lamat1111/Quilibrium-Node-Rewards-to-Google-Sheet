7497c3e3bb24c8b638330506f9d418a09ab1a896
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-07-13 12:47:29 +0200

    bugs

--- qnode_rewards_to_gsheet_custom_2.py
+++ qnode_rewards_to_gsheet_custom_2.py
@@ -67,21 +67,21 @@ def get_time_taken():
         print(f"Error occurred while fetching time taken: {e}")
         return None
 
-def find_next_empty_row(sheet, column, start_row=START_ROW):
+def find_next_empty_row(sheet, column, start_row):
     values_list = sheet.col_values(gspread.utils.a1_to_rowcol(column + '1')[1])
     for i, value in enumerate(values_list[start_row-1:], start=start_row):
         if value == '':
             return i
     return len(values_list) + 1
 
-def update_google_sheet(value, sheet_tab_name, column):
+def update_google_sheet(value, sheet_tab_name, column, start_row):
     try:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         creds = ServiceAccountCredentials.from_json_keyfile_name(AUTH_FILE_PATH, scope)
         client = gspread.authorize(creds)
         sheet = client.open(SHEET_NAME).worksheet(sheet_tab_name)
 
-        empty_row = find_next_empty_row(sheet, column)
+        empty_row = find_next_empty_row(sheet, column, start_row)
         cell = f"{column}{empty_row}"
         sheet.update_acell(cell, value)
         print(f"Value updated successfully: {value} at {cell} in {sheet_tab_name}")
@@ -100,14 +100,14 @@ if __name__ == "__main__":
     command = f"cd ~/ceremonyclient/node && ./{config['NODE_BINARY']} -balance"
     balance = get_balance(command)
     if balance is not None:
-        update_google_sheet(balance, SHEET_REWARDS_TAB_NAME, START_COLUMN)
+        update_google_sheet(balance, SHEET_REWARDS_TAB_NAME, START_COLUMN, START_ROW)
 
     # Get increment
     increment = get_increment()
     if increment is not None:
-        update_google_sheet(increment, SHEET_INCREMENT_TAB_NAME, START_COLUMN)
+        update_google_sheet(increment, SHEET_INCREMENT_TAB_NAME, START_COLUMN, START_ROW)
 
     # Get time taken
     time_taken = get_time_taken()
     if time_taken is not None:
-        update_google_sheet(time_taken, SHEET_TIME_TAKEN_TAB_NAME, START_COLUMN)
+        update_google_sheet(time_taken, SHEET_TIME_TAKEN_TAB_NAME, START_COLUMN, START_ROW)
