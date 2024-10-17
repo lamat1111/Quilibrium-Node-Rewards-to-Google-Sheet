e3cd9840be9b23df854c07876cb02fe86c05f631
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-10-17 18:35:55 +0200

    commented out parts relative to INCREMENTS

--- qnode_rewards_to_gsheet_custom_2.py
+++ qnode_rewards_to_gsheet_custom_2.py
@@ -22,7 +22,7 @@ def read_config(config_file):
 config = read_config(CONFIG_FILE_PATH)
 SHEET_NAME = config.get('SHEET_NAME', 'Quilibrium nodes')
 SHEET_REWARDS_TAB_NAME = config.get('SHEET_REWARDS_TAB_NAME', 'Rewards 2')
-SHEET_INCREMENT_TAB_NAME = config.get('SHEET_INCREMENT_TAB_NAME', 'Increment')
+# SHEET_INCREMENT_TAB_NAME = config.get('SHEET_INCREMENT_TAB_NAME', 'Increment')
 SHEET_TIME_TAKEN_TAB_NAME = config.get('SHEET_TIME_TAKEN_TAB_NAME', 'Time taken')
 START_COLUMN = config.get('START_COLUMN', 'B')
 START_ROW = max(2, int(config.get('START_ROW', '2')))  # Ensure START_ROW is at least 2
@@ -38,14 +38,14 @@ def get_balance(command):
         print(f"Error occurred while fetching balance: {e}")
         return None
 
-def get_increment():
-    command = "sudo journalctl -u ceremonyclient.service --no-hostname -o cat | grep \"\\\"msg\\\":\\\"completed duration proof\\\"\" | tail -n 1 | jq -r \".increment\""
-    try:
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        return int(result.stdout.strip())
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while fetching increment: {e}")
-        return None
+# def get_increment():
+#     command = "sudo journalctl -u ceremonyclient.service --no-hostname -o cat | grep \"\\\"msg\\\":\\\"completed duration proof\\\"\" | tail -n 1 | jq -r \".increment\""
+#     try:
+#         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+#         return int(result.stdout.strip())
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error occurred while fetching increment: {e}")
+#         return None
 
 def get_time_taken():
     command = "sudo journalctl -u ceremonyclient.service --no-hostname -o cat | grep \"\\\"msg\\\":\\\"completed duration proof\\\"\" | tail -n 1 | jq -r \".time_taken\""
@@ -89,9 +89,9 @@ if __name__ == "__main__":
         update_google_sheet(balance, SHEET_REWARDS_TAB_NAME, START_COLUMN, START_ROW)
 
     # Get increment
-    increment = get_increment()
-    if increment is not None:
-        update_google_sheet(increment, SHEET_INCREMENT_TAB_NAME, START_COLUMN, START_ROW)
+    # increment = get_increment()
+    # if increment is not None:
+    #     update_google_sheet(increment, SHEET_INCREMENT_TAB_NAME, START_COLUMN, START_ROW)
 
     # Get time taken
     time_taken = get_time_taken()
